1643f5adaa09ba69d4a6e1bfae70b92d17bbb5f9
Niceleyy <david.wu@edu.univ-paris13.fr>
2024-06-12 11:17:46 +0200

    caca

---
+++ projet_react/src/pageaccueil/Accueil.js
@@ -0,0 +1,26 @@
+// Mini-React-Project/projet_react/src/Home.js
+import React from 'react';
+import Article from './Article';
+import articles from './ListeArticle/ListeArticle';
+
+function Accueil() {
+
+  return (
+    <div className="container mt-5">
+    <div class="header"> 
+        <div class="left">
+            <a>
+                <img alt="Logo myEfrei" 
+                src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Languages/French/LOGO_EFREI-WEB_blanc.png"></img>
+            </a>
+        </div>
+    </div>
+      <h1 className="mb-3">Page d'Accueil</h1>
+      {articles.map(article => (
+        <Article key={article.id} title={article.title} content={article.content} />
+      ))}
+    </div>
+  );
+}
+
+export default Accueil;
---
+++ projet_react/src/pageaccueil/ListeArticle/ListeArticle.js
@@ -0,0 +1,6 @@
+const articles = [
+    { id: 1, title: 'Article 1', content: 'Contenu de l\'article 1...' },
+    { id: 2, title: 'Article 2', content: 'Contenu de l\'article 2...' },
+];
+
+export default articles
