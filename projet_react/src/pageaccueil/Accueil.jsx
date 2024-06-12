8281920e7135572500b859bc1801af380061a6d6
Ludovic MARIE <ludovic.marie@aol.com>
2024-06-12 14:57:17 +0200

    fixed footer

--- projet_react/src/pageaccueil/Accueil.jsx
+++ projet_react/src/pageaccueil/Accueil.jsx
@@ -8,7 +8,7 @@ import Reseau from './Reseau';
 function Accueil() {
 
     return (
-        <div>
+        <div className='accueil'>
             <h1 className="mb-3">Page d'Accueil</h1>
             <div class="center">
                 <div class="contour">
